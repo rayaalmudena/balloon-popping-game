a5fea1bb258fe1eae40a83554c18b2aced0fe052
rayaalmudena <raya.almudena@gmail.com>
2022-03-10 13:55:36 +0100

    por fin

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // tu código aquí
+const balloons = document.querySelectorAll('.balloon');
+let numBallons =balloons.length;
+
+
+balloons.forEach(balloon => {
+    balloon.addEventListener("mouseenter", function pop (event) {
+    
+        balloon.innerHTML="POP!";
+        event.target.style.background = 'transparent';
+        event.target.removeEventListener("mouseenter", pop) ;
+
+        numBallons--;
+        if(numBallons==0){
+            setTimeout(congratzs, 500);           
+        }
+    });
+});
+
+function congratzs(){
+    document.querySelector("#yay-no-balloons").style.display = "block";
+    document.querySelector("#balloon-gallery").style.display = "none";
+}
+
